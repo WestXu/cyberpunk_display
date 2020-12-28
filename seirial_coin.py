63bfaa6ac6e5d973a6fd26fb55655b61f156d0fc
lxu <lxu@junzhiam.com>
2020-12-28 13:39:19 +0800

    serial_coin: 接入BTC数据

--- seirial_coin.py
+++ seirial_coin.py
@@ -1,5 +1,6 @@
+import asyncio
 import serial
-import random
+from ws_coin import Huobi
 
 
 def to_bytes(line1=' ' * 16, line2=' ' * 16):
@@ -10,8 +11,26 @@ def to_bytes(line1=' ' * 16, line2=' ' * 16):
     return b'\xfeH' + line1.encode() + b'\x00\x00\x00\x00' + line2.encode() + b'\xff'
 
 
-with serial.Serial('COM3') as ser:
-    while True:
-        h = to_bytes(f'BTC:{random.randint(0, 9999999) / 100:>12.2f}')
-        ser.write(h)
-        print('sent', h)
+async def main():
+    hb = Huobi()
+    await hb._connect()
+
+    with serial.Serial('COM3') as ser:
+        p = 0
+        trend: Literal['+', '-'] = '+'
+        while True:
+            try:
+                new_p = await hb.recv_price(timeout=0.5)
+                if new_p > p:
+                    trend = '+'
+                elif new_p < p:
+                    trend = '-'
+                p = new_p
+            except asyncio.TimeoutError:
+                pass
+            print(f"{p} {trend}\r", end='', flush=True)
+            ser.write(to_bytes(f"      BTC {trend}     ", f"{p:^16.2f}"))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
