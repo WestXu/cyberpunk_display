1ac17b47538566e2d98452ffd32c42e548f1a351
lxu <lxu@junzhiam.com>
2020-12-28 17:26:53 +0800

    serial_coin: little refactor

--- seirial_coin.py
+++ seirial_coin.py
@@ -1,12 +1,18 @@
 import asyncio
 import serial
 from ws_coin import Huobi
+from typing import Literal
 
 
 def to_bytes(line1=' ' * 16, line2=' ' * 16):
     assert isinstance(line1, str) and isinstance(line2, str)
     assert len(line1) == 16 and len(line2) == 16, f"{len(line1) =}, {len(line2) =}"
     return b'\xfeH' + line1.encode() + b'\x00\x00\x00\x00' + line2.encode() + b'\xff'
+
+
+def send_p(ser, p, trend: Literal['+', '-']):
+    print(f"{p} {trend}\r", end='', flush=True)
+    ser.write(to_bytes(f"      BTC {trend}     ", f"{p:^16.2f}"))
 
 
 async def main():
@@ -26,8 +32,8 @@ async def main():
                 p = new_p
             except asyncio.TimeoutError:
                 pass
-            print(f"{p} {trend}\r", end='', flush=True)
-            ser.write(to_bytes(f"      BTC {trend}     ", f"{p:^16.2f}"))
+
+            send_p(ser, p, trend)
 
 
 if __name__ == "__main__":
