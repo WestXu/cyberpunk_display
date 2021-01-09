49bb102d923b614248d20e71e4b816cb31d998c4
WestXu <xu-lai-xi@qq.com>
2021-01-10 01:06:34 +0800

    awtrix: refactor

--- awtrix.py
+++ awtrix.py
@@ -8,51 +8,63 @@ from loguru import logger
 from ws_coin import Huobi
 
 
-async def push(ssn: aiohttp.ClientSession, data: dict, endpoint: str):
-    async with ssn.post(
-        f'http://localhost:7000/api/v3/{endpoint}',
-        data=json.dumps(data),
-        headers={'Content-Type': 'application/json'},
-    ) as res:
-        return res.text
-
-
-async def draw_exit(ssn: aiohttp.ClientSession):
-    await push(
-        ssn,
-        {
-            "draw": [{"type": "exit"}],
-        },
-        endpoint='draw',
-    )
-
-
-async def draw_price(ssn: aiohttp.ClientSession, price):
-    str_price = str(price)
-    await push(
-        ssn,
-        {
-            "draw": [
-                {"type": "fill", "color": [50, 50, 50]},
-                {
-                    "type": "text",
-                    "string": str_price,
-                    "position": [1, 1],
-                    "color": [255, 255, 255],
-                },
-                {"type": "show"},
-            ],
-        },
-        endpoint='draw',
-    )
-
-
 class Awtrix:
-    def __init__(self, loop) -> None:
-        self._ssn = aiohttp.ClientSession()
+    def __init__(self) -> None:
         self._q: asyncio.Queue = asyncio.Queue(maxsize=1)
 
-        self.loop = loop
+    async def __aenter__(self):
+        self._ssn = aiohttp.ClientSession()
+        await self.draw_background()
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self.draw_exit()
+        await self._ssn.close()
+
+    async def _push(self, data: dict, endpoint: str):
+        async with self._ssn.post(
+            f'http://localhost:7000/api/v3/{endpoint}',
+            data=json.dumps(data),
+            headers={'Content-Type': 'application/json'},
+        ) as res:
+            return res.text
+
+    async def draw_background(self):
+        await self._push(
+            {
+                "draw": [
+                    {"type": "fill", "color": [100, 100, 100]},
+                    {"type": "show"},
+                ]
+            },
+            endpoint='draw',
+        )
+
+    async def draw_exit(self):
+        await self._push(
+            {
+                "draw": [{"type": "exit"}],
+            },
+            endpoint='draw',
+        )
+
+    async def draw_price(self, price):
+        str_price = str(price)
+        await self._push(
+            {
+                "draw": [
+                    {"type": "fill", "color": [50, 50, 50]},
+                    {
+                        "type": "text",
+                        "string": str_price,
+                        "position": [1, 1],
+                        "color": [255, 255, 255],
+                    },
+                    {"type": "show"},
+                ],
+            },
+            endpoint='draw',
+        )
 
     async def update(self, p):
         if not self._q.empty():
@@ -61,7 +73,7 @@ class Awtrix:
         await self._q.put(p)
 
     async def send(self, p):
-        await draw_price(self._ssn, p)
+        await self.draw_price(p)
         logger.info(f'Sent to awtrix')
 
     async def send_latest(self):
@@ -86,8 +98,10 @@ async def pushing(awtrix: Awtrix):
 
 if __name__ == "__main__":
 
-    loop = asyncio.get_event_loop()
-    awtrix = Awtrix(loop)
+    async def main(loop):
+        async with Awtrix() as awtrix:
+            loop.create_task(data(awtrix))
+            await pushing(awtrix)
 
-    loop.create_task(data(awtrix))
-    loop.run_until_complete(pushing(awtrix))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
