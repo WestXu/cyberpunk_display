e0fc69a5034bc4aff17498976ea242c3f663e3f8
WestXu <xu-lai-xi@qq.com>
2021-01-10 00:17:47 +0800

    awtrix: perf using session

--- awtrix.py
+++ awtrix.py
@@ -4,18 +4,18 @@ import json
 import aiohttp
 
 
-async def push(data: dict, endpoint: str):
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-            f'http://localhost:7000/api/v3/{endpoint}',
-            data=json.dumps(data),
-            headers={'Content-Type': 'application/json'},
-        ) as res:
-            return res.text
+async def push(ssn: aiohttp.ClientSession, data: dict, endpoint: str):
+    async with ssn.post(
+        f'http://localhost:7000/api/v3/{endpoint}',
+        data=json.dumps(data),
+        headers={'Content-Type': 'application/json'},
+    ) as res:
+        return res.text
 
 
-async def draw_exit():
+async def draw_exit(ssn: aiohttp.ClientSession):
     await push(
+        ssn,
         {
             "draw": [{"type": "exit"}],
         },
@@ -23,10 +23,11 @@ async def draw_exit():
     )
 
 
-async def draw_price(price: int):
+async def draw_price(ssn: aiohttp.ClientSession, price: int):
     str_price = str(price)
     assert len(str_price) == 5
     await push(
+        ssn,
         {
             "draw": [
                 {"type": "fill", "color": [50, 50, 50]},
@@ -46,10 +47,11 @@ async def draw_price(price: int):
 if __name__ == "__main__":
 
     async def main():
-        for i in range(1, 9):
-            await draw_price(i * 11111)
-            await asyncio.sleep(0.1)
+        async with aiohttp.ClientSession() as ssn:
+            for i in range(1, 9):
+                await draw_price(ssn, i * 11111)
+                await asyncio.sleep(0.1)
 
-        await draw_exit()
+            await draw_exit(ssn)
 
     asyncio.run(main())
