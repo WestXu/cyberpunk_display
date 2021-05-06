81949472239ac80a562d18b77cf55d3bd667fbf5
lxu <lxu@junzhiam.com>
2021-05-06 15:02:24 +0800

    ws_coin: ws -> wss

--- ws_coin.py
+++ ws_coin.py
@@ -8,7 +8,7 @@ from loguru import logger
 
 
 class Huobi:
-    uri = "ws://api.hadax.com/ws"
+    uri = "wss://api.hadax.com/ws"
 
     def __init__(self, markets: List[str]) -> None:
         self.markets = markets
