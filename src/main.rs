caaf3be1c0c9f6a69b489b1191c1d3ab1834cc7b
lxu <lxu@junzhiam.com>
2021-06-09 14:27:22 +0800

    print rand p

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+
+    let mut p = 50000.0;
+
+    for _i in 1..20 {
+        p += rng.gen_range(-1000.0..1000.0);
+        println!("{:2}", p);
+    }
 }
