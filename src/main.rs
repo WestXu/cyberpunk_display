6d9bae6efb2ffe51903d0050103af788b4ce2377
WestXu <xu-lai-xi@qq.com>
2021-06-21 21:40:34 +0800

    implement WsCoin using iterator

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,10 @@
-use tungstenite::{connect, Message};
-use url::Url;
-
-use flate2::read::GzDecoder;
-use std::io::Read;
-
 use cyberpunk_display::price_queue::PriceQueue;
-use cyberpunk_display::ws_coin::{
-    parse_json::{parse_json, Msg},
-    pong,
-};
+use cyberpunk_display::ws_coin::WsCoin;
 
 fn main() {
     let mut pq = PriceQueue::default();
-
-    let (mut socket, response) =
-        connect(Url::parse("wss://api.hadax.com/ws").unwrap()).expect("Can't connect");
-
-    for (ref header, _value) in response.headers() {
-        println!("* {}", header);
+    for p in WsCoin::default() {
+        pq.push(p);
+        println!("{}", pq)
     }
-
-    socket
-        .write_message(Message::Text(
-            r#"{"sub": "market.btcusdt.trade.detail", "id": "btcusdt"}"#.into(),
-        ))
-        .unwrap();
-    loop {
-        let msg = socket.read_message().expect("Error reading message");
-        let msg_binary = msg.into_data();
-        let mut gz = GzDecoder::new(&msg_binary[..]);
-        let mut s = String::new();
-        gz.read_to_string(&mut s).unwrap();
-        match parse_json(&s) {
-            Msg::Ping(ping) => {
-                pong(&mut socket, ping);
-                continue;
-            }
-            Msg::Subscribed(ch) => println!("Parsed: {:?}", ch),
-            Msg::Price(p) => {
-                pq.push(p);
-                println!("{}\n", pq)
-            }
-        }
-    }
-    // socket.close(None);
 }
--- src/ws_coin/mod.rs
+++ src/ws_coin/mod.rs
@@ -1,6 +1,64 @@
 pub mod parse_json;
 
+use tungstenite::{connect, Message};
+use url::Url;
+
+use ordered_float::NotNan;
 use serde_json::json;
+
+use flate2::read::GzDecoder;
+use std::io::Read;
+
+use parse_json::{parse_json, Msg};
+
+pub struct WsCoin {
+    socket: tungstenite::WebSocket<
+        tungstenite::stream::Stream<
+            std::net::TcpStream,
+            native_tls::TlsStream<std::net::TcpStream>,
+        >,
+    >,
+}
+
+impl Default for WsCoin {
+    fn default() -> Self {
+        let (mut socket, _) =
+            connect(Url::parse("wss://api.hadax.com/ws").unwrap()).expect("Can't connect");
+
+        socket
+            .write_message(Message::Text(
+                r#"{"sub": "market.btcusdt.trade.detail", "id": "btcusdt"}"#.into(),
+            ))
+            .unwrap();
+
+        WsCoin { socket }
+    }
+}
+
+impl Iterator for WsCoin {
+    type Item = NotNan<f64>;
+    fn next(&mut self) -> Option<Self::Item> {
+        let msg = self.socket.read_message().expect("Error reading message");
+        let msg_binary = msg.into_data();
+        let mut gz = GzDecoder::new(&msg_binary[..]);
+        let mut s = String::new();
+        gz.read_to_string(&mut s).unwrap();
+        match parse_json(&s) {
+            Msg::Ping(ping) => {
+                pong(&mut self.socket, ping);
+                self.next()
+            }
+            Msg::Subscribed(ch) => {
+                println!("Parsed: {:?}", ch);
+                self.next()
+            }
+            Msg::Price(p) => {
+                println!("Parsed: {:?}", p);
+                Some(p)
+            }
+        }
+    }
+}
 
 pub fn pong(
     socket: &mut tungstenite::WebSocket<
