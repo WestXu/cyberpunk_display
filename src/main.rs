3b64443708c5f1b39edf904320e827482a659d11
WestXu <xu-lai-xi@qq.com>
2021-06-21 17:05:20 +0800

    now sucess on receiving data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use ordered_float::NotNan;
 use rand::Rng;
+use tungstenite::{connect, Message};
+use url::Url;
+
+use flate2::read::GzDecoder;
+use std::io::Read;
 
 use cyberpunk_display::price_queue::PriceQueue;
 
@@ -16,4 +21,29 @@ fn main() {
         println!("{:}", pq);
         println!("\n");
     }
+
+    env_logger::init();
+    let (mut socket, response) =
+        connect(Url::parse("wss://api.hadax.com/ws").unwrap()).expect("Can't connect");
+    println!("Connected to the server");
+    println!("Response HTTP code: {}", response.status());
+    println!("Response contains the following headers:");
+    for (ref header, _value) in response.headers() {
+        println!("* {}", header);
+    }
+
+    socket
+        .write_message(Message::Text(
+            r#"{"sub": "market.btcusdt.trade.detail", "id": "btcusdt"}"#.into(),
+        ))
+        .unwrap();
+    loop {
+        let msg = socket.read_message().expect("Error reading message");
+        let msg_binary = msg.into_data();
+        let mut gz = GzDecoder::new(&msg_binary[..]);
+        let mut s = String::new();
+        gz.read_to_string(&mut s).unwrap();
+        println!("Received: {}", s);
+    }
+    // socket.close(None);
 }
